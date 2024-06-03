e2411f4f14265945832ec46c52f7d905f9da703d
Akshay Dohroo <akshaydohroo@Akshays-MacBook-Air.local>
2024-06-03 08:59:08 +0530

    first commit

---
+++ src/main/java/org/example/json/Listener.java
@@ -0,0 +1,70 @@
+package org.example.json;
+
+import java.util.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.io.IOException;
+
+public class Visitor extends JSONBaseListener {
+
+
+    private Object result;
+    private Stack<Object> stack = new Stack<>();
+
+    public Object getResult() {
+        return result;
+    }
+
+    @Override
+    public void enterJson(JSONParser.JsonContext ctx) {
+        stack.clear();
+    }
+
+    @Override
+    public void exitJson(JSONParser.JsonContext ctx) {
+        result = stack.pop();
+    }
+
+    @Override
+    public void enterObj(JSONParser.ObjContext ctx) {
+        stack.push(new HashMap<>());
+    }
+
+    @Override
+    public void exitPair(JSONParser.PairContext ctx) {
+        Object value = stack.pop();
+        String key = ctx.STRING().getText().replaceAll("^\"|\"$", "");
+        Map<String, Object> obj = (Map<String, Object>) stack.peek();
+        obj.put(key, value);
+    }
+
+    @Override
+    public void exitArr(JSONParser.ArrContext ctx) {
+        List<Object> array = new ArrayList<>();
+        while (!stack.isEmpty() && !(stack.peek() instanceof Map || stack.peek() instanceof List)) {
+            array.add(0, stack.pop());
+        }
+        stack.push(array);
+    }
+
+    @Override
+    public void exitValue(JSONParser.ValueContext ctx) {
+        if (ctx.STRING() != null) {
+            stack.push(ctx.STRING().getText().replaceAll("^\"|\"$", ""));
+        } else if (ctx.NUMBER() != null) {
+            stack.push(Double.parseDouble(ctx.NUMBER().getText()));
+        } else if (ctx.obj() != null || ctx.arr() != null) {
+            // Object or array is already on the stack
+        } else if (ctx.getText().equals("true")) {
+            stack.push(true);
+        } else if (ctx.getText().equals("false")) {
+            stack.push(false);
+        } else if (ctx.getText().equals("null")) {
+            stack.push(null);
+        }
+    }
+}
+
+
---
+++ src/main/java/org/example/json/TreeUtils.java
@@ -0,0 +1,2 @@
+package org.example.json;public class TreeUtils {
+}
